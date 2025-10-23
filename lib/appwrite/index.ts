82a9485bfbd36fdc1bdcd873f1827fe2765fc34b
SIMPLESOMEONE1202 <rudraraj.krishna@gmail.com>
2025-10-24 01:36:07 +0530

    Clean commit by Rudra

---
+++ lib/appwrite/index.ts
@@ -0,0 +1,51 @@
+"use server";
+
+import { Account, Avatars, Client, Databases, Storage } from "node-appwrite";
+import { appwriteConfig } from "@/lib/appwrite/config";
+import { cookies } from "next/headers";
+
+export const createSessionClient = async () => {
+  const cookieStore = await cookies(); // ✅ await here
+  const session = cookieStore.get("appwrite-session");
+
+  if (!session?.value) throw new Error("No session");
+
+  const client = new Client()
+    .setEndpoint(appwriteConfig.endpointUrl)
+    .setProject(appwriteConfig.projectId)
+    .setSession(session.value);
+
+  return {
+    get account() {
+      return new Account(client);
+    },
+    get databases() {
+      return new Databases(client);
+    },
+    get storage() {
+      return new Storage(client);
+    },
+  };
+};
+
+export const createAdminClient = async () => {
+  const client = new Client()
+    .setEndpoint(appwriteConfig.endpointUrl)
+    .setProject(appwriteConfig.projectId)
+    .setKey(appwriteConfig.secretKey);
+
+  return {
+    get account() {
+      return new Account(client);
+    },
+    get databases() {
+      return new Databases(client);
+    },
+    get storage() {
+      return new Storage(client);
+    },
+    get avatars() {
+      return new Avatars(client);
+    },
+  };
+};
